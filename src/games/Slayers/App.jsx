51f6bf04203da3a041ad16282cd6087bbc51cc0a
SkyMazter <32372450+SkyMazter@users.noreply.github.com>
2020-05-06 18:37:18 -0400

    added data model
    players and boss now have health

--- src/games/Slayers/App.jsx
+++ src/games/Slayers/App.jsx
@@ -1,15 +1,49 @@
 import GameComponent from "../../GameComponent.js";
 import React from "react";
 import UserApi from "../../UserApi.js";
+import MainGame from "./MainGame.jsx";
 
 export default class Slayers extends GameComponent {
   constructor() {
     super();
     this.state = {
-      render: null
+      render: null,
+      playerOneHealth: null,
+      playerTwoHealth: null,
+      dragonHealth: null,
+      activeTurn: null
     };
+    this.attack = this.attack.bind(this);
+    var myid = this.getMyUserId();
+    var creator_id = this.getSessionCreatorUserId();
+    if (creator_id === myid) {
+      this.getSessionDatabaseRef().set({
+        playerOneHealth: 1000,
+        playerTwoHealth: 1000,
+        dragonHealth: 4000,
+        activeTurn: "player1"
+      });
+      // this.setState({
+      //   playerHealth: 1000
+      // })
+    } else {
+      // host_greeting = "You are not the host";
+    }
   }
 
+  onSessionDataChanged(dataChanged) {
+    this.setState({
+      playerOneHealth: dataChanged.playerOneHealth,
+      playerTwoHealth: dataChanged.playerTwoHealth,
+      dragonHealth: dataChanged.dragonHealth,
+      activeTurn: dataChanged.activeTurn
+    });
+  }
+  attack() {
+    this.getSessionDatabaseRef().set({
+      dragonHealth: this.state.dragonHealth - 100
+    });
+  }
   render() {
     var id = this.getSessionId();
     var users = this.getSessionUserIds().map(user_id => (
@@ -25,10 +59,16 @@ export default class Slayers extends GameComponent {
     var myid = this.getMyUserId();
     var host_greeting = null;
 
-    if (creator_id === myid) {
-      host_greeting = "You are the host";
-    } else {
-      host_greeting = "You are not the host";
+    if (this.state.render === "characterSelect") {
+      return (
+        <MainGame
+          playerOneHealth={this.state.playerOneHealth}
+          playerTwoHealth={this.state.playerTwoHealth}
+          dragonHealth={this.state.dragonHealth}
+          activeTurn={this.state.activeTurn}
+          attack={this.attack}
+        />
+      );
     }
     return (
       <div>
@@ -40,7 +80,13 @@ export default class Slayers extends GameComponent {
         <h1>Slayers</h1>
         <p>Player 1: {creator}</p>
         <p>Player 2: {users[1]}</p>
-        <button onClick={() => {}}>Start Game</button>
+        <button
+          onClick={() => {
+            this.setState({ render: "characterSelect" });
+          }}
+        >
+          Start Game
+        </button>
       </div>
     );
   }
---
+++ src/games/Slayers/MainGame.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class MainGame extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.dragonHealth}</h1>
+        <button
+          onClick={() => {
+            this.props.attack();
+          }}
+        />
+      </div>
+    );
+  }
+}
